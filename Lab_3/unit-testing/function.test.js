034e21dbf42c4f0b0850f5fae3bc9982df9dab81
SerhiyMykuliak <serhijko90@gmail.com>
2025-03-06 19:16:39 +0200

    add unit test for fromCharCode

--- Lab_3/unit-testing/function.test.js
+++ Lab_3/unit-testing/function.test.js
@@ -12,29 +12,32 @@ describe('fromCharCode', () => {
     expect(fromCharCode(0x269B)).toBe('⚛')
   })
 
-  describe('padStart', () => {
-    test('should pad the string at the start with specified chars', () =>{
-      expect(padStart("lo", 5, "Hel")).toBe("Hello")
-      expect(padStart("lake", 10, "~")).toBe("~~~~~~lake")
-    })
+  test('should return NaN for invalid input', () =>{
+    expect(fromCharCode("F")).toBe("\u0000")
+  })
+})
 
-    test('should return an unchanged string if the length is sufficient', () =>{
-      expect(padStart("Hello", 5, "!")).toBe("Hello")
-      expect(padStart("cycle", 3, "Motor")).toBe("cycle")
-    })
+describe('padStart', () => {
+  test('should pad the string at the start with specified chars', () =>{
+    expect(padStart("lo", 5, "Hel")).toBe("Hello")
+    expect(padStart("lake", 10, "~")).toBe("~~~~~~lake")
   })
 
-  describe('padEnd', () => {
-    test('should pad the string at the end with specified chars', () =>{
-      expect(padEnd("4", 5, "0")).toBe("40000")
-      expect(padEnd("Motor", 10, "cycle")).toBe("Motorcycle")
-    })
-
-    test('should return an unchanged string if the length is sufficient', () =>{
-      expect(padEnd("Hello", 5, "!!")).toBe("Hello")
-      expect(padEnd("cycle", 3, "Motor")).toBe("cycle")
-    })
+  test('should return an unchanged string if the length is sufficient', () =>{
+    expect(padStart("Hello", 5, "!")).toBe("Hello")
+    expect(padStart("cycle", 3, "Motor")).toBe("cycle")
   })
-  
+})
+
+describe('padEnd', () => {
+  test('should pad the string at the end with specified chars', () =>{
+    expect(padEnd("4", 5, "0")).toBe("40000")
+    expect(padEnd("Motor", 10, "cycle")).toBe("Motorcycle")
+  })
+
+  test('should return an unchanged string if the length is sufficient', () =>{
+    expect(padEnd("Hello", 5, "!!")).toBe("Hello")
+    expect(padEnd("cycle", 3, "Motor")).toBe("cycle")
+  })
 })
 
